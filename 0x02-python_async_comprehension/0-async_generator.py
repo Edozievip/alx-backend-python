8cd11c52ea5fc0abf331275c26b84ac62526ada3
Ezeh Christian Chiedozie <edozievip898@gmail.com>
2023-10-11 05:05:34 +0100

    initial commit

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''Async Generator module
+'''
+
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator():
+	'''This coroutine loops 10*, waits for 1 sec and yields a random number between 0 and 10'''
+	for i in range(10):
+		random = random.random() * 10
+		await async.sleep(1)
+		yield random
+	
