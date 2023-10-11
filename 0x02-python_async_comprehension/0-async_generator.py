d8a11da0b3b1b3436baa88e2f3b1c8a4645dd391
Ezeh Christian Chiedozie <edozievip898@gmail.com>
2023-10-11 05:37:04 +0100

    final commit

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
-'''Async Generator module
+'''Async Generator module.
 '''
-
 import asyncio
 import random
 from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-	'''This coroutine loops 10*, waits for 1 sec and yields a random number between 0 and 10'''
-	for _ in range(10):
-		random = random.random() * 10
-		await asyncio.sleep(1)
-		yield random
-	
+    '''This loops 10*, waits a sec yields a random number between 0 and 10.
+    '''
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-'''Task 2's module.
+'''Parallel comprehension.
 '''
 import asyncio
 import time
